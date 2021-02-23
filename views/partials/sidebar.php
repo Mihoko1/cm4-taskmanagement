3dc703dcbb0806cd51044b1c487bc7e9e0b111bd
Hui Ser Choo <choohuiser@yahoo.com>
2021-02-23 02:30:19 -0500

    Connect to boostrap and set up table but not the sidebar nav

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,22 +1,53 @@
 <?php
 
 // The insertSidebar() function creates a sidebar and navigation menu plus related elements. The function will accept 1 argument as parameter: Navigation menu items (associative array). If no arguments are supplied, the function will use its own default values. The function outputs the HTML code for the footer.
-function insertSidebar($navItemsParam = ['New Project' => '/views/createproject.php', 
-                                        'Search Project' => '/views/searchprojects.php']) {
+function insertSidebar($pageTitleParam = 'Task Management',
+                        $navItemsParam = ['New Project' => '/views/createproject.php',
+                                        'Search Project' => '/views/searchprojects.php',
+                                        'Link 3' => '/views/link3.php',
+                                        'Link 4' => '/views/Link4.php',
+                                        'Link 5' => '/views/Lnk5.php'],
+                       $cssPathParam = '../style/global.css',
+                       $jsPathParam = '',
+                       $bootstrapCssPathParam = '../css/bootstrap.min.css',
+                       $bootstrapJsPathParam = '../js/bootstrap.min.js')  {
 
+
+    $pageTitle = $pageTitleParam;
     $navItems = $navItemsParam;
+    $cssPath = $cssPathParam;
+    $jsPath = $jsPathParam;
+    $bootstrapCssPath = $bootstrapCssPathParam;
+    $bootstrapJsPath = $bootstrapJsPathParam;
     $navListItemsString = '';
 
-    // Create the navigation menu list items HTML code
-    foreach ($navItems as $linkName => $Uri) {
-        $navListItemsString .= "<li><a href=\"$Uri\">$linkName</a></li>";
-    }
+        // Create the navigation menu list items HTML code
+        foreach ($navItems as $linkName => $Uri) {
+            $navListItemsString .= "<li class=\"nav-item \"><a class=\"nav-link\" href=\"$Uri\">$linkName</a></li>";
+        }
 
     echo <<<SIDEBAR
-    
-    <aside>
-        <nav>
-            <ul>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width">
+        <script type="text/javascript" src="$jsPath"></script>
+        <link rel="stylesheet" href="$cssPath" />
+        <link rel="stylesheet" href="$bootstrapCssPath" />
+        <script type="text/javascript" src="$bootstrapJsPath"></script>
+        <title>$pageTitle</title>
+    </head>
+    <<body class="d-flex h-100 text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="nav ">
+        <h1><a class="nav-link" href="../../.index.php">
+                    C4M
+                </a></h1>
+    <div class="wrapper">
+        <nav id="sidebar" class="navbar navbar-light ">
+            <ul class="list-unstyled components nav flex-column">
                 $navListItemsString
             </ul>
         </nav>
@@ -26,11 +57,13 @@ function insertSidebar($navItemsParam = ['New Project' => '/views/createproject.
         <div>
             <p>Put Event Calendar Here</p>
         </div>
-    </aside>
+        <div>
+         <a class="btn btn-secondary btn-lg my-5" href="./index.php" role="button"> Logout</a>
+        </div>
+    </header>
     
     SIDEBAR;
 
 }
-
 
 ?>
--- views/task-board.php
+++ views/task-board.php
@@ -1,76 +1,69 @@
 <?php
-
 require("./partials/header.php");
-insertHeader();
+require("./partials/sidebar.php");
+insertSidebar();
 ?>
     <!--Main Start Here-->
     <!--Content Start here-->
-<main class = "container container-login text-center my-5">
-    <div class=" my-5">
-        <h2>TEAM C4M Task Management</h2>
-        <div class="container">
-            <h1>Bootstrap Table</h1>
+<main>
+    <h2 class="my-md-5">Dashboard</h2>
+        <div class="container-fluid ">
+                <div class = "d-lg-flex pb-2 justify-content-end">
+                    <div  class = "col-sm-2 d-inline-flex flex-fill" >
+                        <select class="form-control">
+                            <option value="">Assigned To</option>
+                        </select>
+                    </div>
+                    <div  class = "col-sm-2 d-inline-flex flex-fill">
+                        <select class="form-control">
+                            <option value="">Projects</option>
+                        </select>
+                    </div>
+                    <div  class="col-sm-2 d-inline-flex flex-fill">
+                        <select class="form-control">
+                            <option value="">Status</option>
+                        </select>
+                    </div>
+                    <div  class="col-sm-2 d-inline-flex flex-fill">
+                        <select class="form-control">
+                            <option value="">Sort By</option>
+                        </select>
+                    </div>
+                </div>
+            <p></p>
 
-            <div id="toolbar" class = "col-1">
-                <select class="form-control">
-                    <option value="">Assigned To</option>
-                </select>
-            </div>
-            <div id="toolbar" class = "col-1">
-                <select class="form-control">
-                    <option value="">Projects</option>
-                </select>
-            </div>
-            <div id="toolbar">
-                <select class="form-control">
-                    <option value="">States</option>
-                </select>
-            </div>
-            <div id="toolbar">
-                <select class="form-control">
-                    <option value="">Sort By</option>
-                </select>
-            </div>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th data-field="id" >ID</th>
+                        <th data-field="title" data-filter-control="input" data-sortable="true">Title</th>
+                        <th data-field="project" data-filter-control="select" data-sortable="true">Project</th>
+                        <th data-field="status" data-filter-control="select" data-sortable="true">Status</th>
 
-            <table id="table"
-                   data-toggle="table"
-                   data-search="true"
-                   data-filter-control="true"
-                   data-show-export="true"
-                   data-click-to-select="true"
-                   data-toolbar="#toolbar"
-                   class="table-responsive">
-                <thead>
-                <tr>
-                    <th data-field="id" >ID</th>
-                    <th data-field="title" data-filter-control="input" data-sortable="true">Title</th>
-                    <th data-field="project" data-filter-control="select" data-sortable="true">Project</th>
-                    <th data-field="status" data-filter-control="select" data-sortable="true">Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Aroma</td>
+                        <td>Restaurant Website</td>
+                        <td>Ongoing</td>
+                    </tr>
+                    <tr>
+                        <td> 2</td>
+                        <td>PHP</td>
+                        <td>Task Management System</td>
+                        <td>Ongoing</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        <!--Main End Here-->
+    </main>
 
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Aroma</td>
-                    <td>Restaurant Website</td>
-                    <td>Ongoing</td>
-                </tr>
-                <tr>
-                    <td> 2</td>
-                    <td>PHP</td>
-                    <td>Task Mangement System</td>
-                    <td>Ongoing</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    <!--Main End Here-->
-</main>
-
-            <footer class="text-center mt-auto ">
-                <p>&#169 Copyright 2021, Team C4M.</p>
-            </footer>
-        </div>
-    </body>
+    <footer class="text-center mt-auto ">
+        <p>&#169 Copyright 2021, Team C4M.</p>
+    </footer>
+</div>
+</body>
 </html>
