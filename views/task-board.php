cd650ff41bca17b2bd41a98fd57cc20e24f448b6
Hui Ser Choo <choohuiser@yahoo.com>
2021-02-23 04:04:52 -0500

    Refill the css for task-board page

--- views/task-board.php
+++ views/task-board.php
@@ -8,38 +8,38 @@ insertHeader();
 <main class = "container container-login text-center my-5">
     <div class=" my-5">
         <h2>TEAM C4M Task Management</h2>
-        <div class="container">
-            <h1>Bootstrap Table</h1>
+        <h1>Bootstrap Table</h1>
+        <div class="container-fluid d-lg-flex pb-5  "></div>
 
-            <div id="toolbar" class = "col-1">
+            <div id="toolbar" class = "col-lg-2 d-inline-flex flex-md-fill">
                 <select class="form-control">
                     <option value="">Assigned To</option>
                 </select>
             </div>
-            <div id="toolbar" class = "col-1">
+            <div id="toolbar" class = "col-lg-2 d-inline-flex flex-md-fill">
                 <select class="form-control">
                     <option value="">Projects</option>
                 </select>
             </div>
-            <div id="toolbar">
+            <div id="toolbar" class ="col-lg-2 d-inline-flex flex-md-fill">
                 <select class="form-control">
-                    <option value="">States</option>
+                    <option value="">Status</option>
                 </select>
             </div>
-            <div id="toolbar">
-                <select class="form-control">
+            <div id="toolbar" class ="col-lg-2 d-inline-flex flex-md-fill">
+                <select class="form-control ">
                     <option value="">Sort By</option>
                 </select>
             </div>
-
-            <table id="table"
+        <div  class="table-responsive my-md-5">
+            <table class="table"
                    data-toggle="table"
                    data-search="true"
                    data-filter-control="true"
                    data-show-export="true"
                    data-click-to-select="true"
                    data-toolbar="#toolbar"
-                   class="table-responsive">
+                  >
                 <thead>
                 <tr>
                     <th data-field="id" >ID</th>
@@ -59,7 +59,7 @@ insertHeader();
                 <tr>
                     <td> 2</td>
                     <td>PHP</td>
-                    <td>Task Mangement System</td>
+                    <td>Task Management System</td>
                     <td>Ongoing</td>
                 </tr>
                 </tbody>
