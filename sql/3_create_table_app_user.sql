3f9342fd9569e1c9d41c2cf2503894fd94cd22cd
Hui Ser Choo <choohuiser@yahoo.com>
2021-03-08 13:37:21 -0500

    create contact_info_internal table ; Add emaill address on app_user

--- sql/3_create_table_app_user.sql
+++ sql/3_create_table_app_user.sql
@@ -5,10 +5,11 @@ CREATE TABLE `app_user` (
   `middle_name` varchar(50) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `phone_number` varchar(30) DEFAULT NULL
+  `phone_number` varchar(30) DEFAULT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
 )
 
 
 --Insert Data--
-INSERT INTO `app_user` (`ID`, `first_name`, `last_name`, `middle_name`, `username`, `password`, `phone_number`) 
-VALUES (NULL, 'James', 'Bond', NULL, 'james_bond', '1234567', '641-255-6825');
+INSERT INTO `app_user` (`ID`, `first_name`, `last_name`, `middle_name`, `username`, `password`, `phone_number`,`email_address`) 
+VALUES (NULL, 'James', 'Bond', NULL, 'james_bond', '1234567', '641-255-6825','james.bond@test.com');
