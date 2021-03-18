091611e88b2598271e9c579c786ba85acd59e90d
Hui Ser Choo <choohuiser@yahoo.com>
2021-03-17 22:08:40 -0400

    Project Overview Class and Page added

---
+++ Model/ProjectOverview.php
@@ -0,0 +1,14 @@
+<?php
+// namespace TaskManagement\Model;
+
+class ProjectOverview
+{
+
+    public function getAllProjects($dbcon){
+        $query = "SELECT * FROM project";
+        $pdostm = $dbcon->prepare($query);
+        $pdostm->execute();
+        $projects = $pdostm->fetchAll(PDO::FETCH_OBJ);
+        return $projects;
+    }
+}
