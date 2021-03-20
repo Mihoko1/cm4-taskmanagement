038ed58dc286b23734af9d30cd0088c6ee5db333
Hui Ser Choo <choohuiser@yahoo.com>
2021-03-19 21:52:47 -0400

    update on new-project

--- views/new-project.php
+++ views/new-project.php
@@ -8,10 +8,12 @@ require("./partials/header.php");
 insertHeader();
 //insertSidebar();
 //session_start();
+//require_once './newprojectCheck.php';
 
 ?>
     <!--Main Start Here-->
     <!--Content Start here-->
+<main>
     <div class="container container-login text-center my-5">
         <div class="row justify-content-md-center">
             <h2>Create New Project</h2>
@@ -20,27 +22,31 @@ insertHeader();
                     
                     <div class="form-group row mb-3">
                         <label class="col-sm-3 col-form-label" for="fname">Project Name</label>
-                        <input class="col-sm-9" type="text" name="pname" id="pname" placeholder="Please type your project name">
+                        <input class="col-sm-9" type="text" name="projectName" id="projectName" placeholder="Please type your project name">
+                        <span style="color:red;"><?= isset($projectNameErr) ? $projectNameErr : ''; ?></span>
                     </div>
              
                     
                     <div class="form-group row mb-3">
                         <label class="col-sm-3 col-form-label" for="email">Start Date</label>
                         <input class="col-sm-9" type="text" name="email" id="email" placeholder="Please type your email address">
+                        <span style="color:red;"><?= isset($emailErr) ? $emailToErr : ''; ?></span>
                     </div>
                     
                     <div class="form-group row mb-3">
                         <div class="label"><label class="col-form-label" for="message">Description</label> 
-                        <textarea class="form-control" name="message" id="message" rows="6"></textarea>
+                        <textarea class="form-control" name="projectDesc" id="projectDesc" rows="6"></textarea>
+                        <span style="color:red;"><?= isset($projectDescErr) ? $projectDescErr : ''; ?></span>
                     </div>
 
                    
                     <div class="form-group row mb-3">
-                        <label class="col-sm-3 col-form-label" for="add_member">Member(s)</label>
-                        <input class="col-sm-9" type="" name="password" id="password" placeholder="Please type your password">
+                        <label class="col-sm-3 col-form-label" for="member">Member(s)</label>
+                        <input class="col-sm-9" type="" name="member[]" id="member[]" placeholder="Please Select your member">
+                        <span style="color:red;"><?= isset($members_err) ? $members_err : ''; ?></span>
                     </div>
 
-                    <div class="form-group my-5">
+                    <div class="form-group my-5 text-center">
                         <div>
                         <input type="submit" name ="submit" class="btn btn-primary btn-lg" value="Create" >
                         </div>
@@ -48,3 +54,11 @@ insertHeader();
             </div>
         </div>
     </div>
+    </div>
+</main>
+<?php
+
+require("./partials/footer.php");
+insertFooter();
+
+?>
