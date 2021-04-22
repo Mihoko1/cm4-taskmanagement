c5b230bea7b1241edc9b46f3c68752115cb018c0
Miho <mihoko.s0408@gmail.com>
2021-04-22 13:08:58 -0400

    added clearDB connection

--- Model/Database.php
+++ Model/Database.php
@@ -3,11 +3,24 @@
 
 class Database
 {
+
     //properties
-    private static $user = 'root';
-    private static $pass = 'root';
-    private static $dsn = 'mysql:host=localhost;dbname=task_management';
-    private static $dbcon;
+    // private static $user = 'root';
+    // private static $pass = 'root';
+    // private static $dsn = 'mysql:host=localhost;dbname=task_management';
+    // private static $dbcon;
+    //mysql://bfaca630ffd158:b64928b4@us-cdbr-east-03.cleardb.com/heroku_96a828e3e46c53e?reconnect=true
+    private static $user = 'bfaca630ffd158';
+    private static $pass = 'b64928b4';
+    private static $dsn = 'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_96a828e3e46c53e';
+  
+
+    //properties
+    // private static $user = 'root';
+    // private static $pass = 'root';
+    // private static $dsn = 'mysql:host=localhost;dbname=task_management';
+    
+     private static $dbcon;
 
     private function __construct()
     {
