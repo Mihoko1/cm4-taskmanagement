f43b4dda02100a1b581443f0fa19bbed655a450b
Hui Ser Choo <choohuiser@yahoo.com>
2021-04-27 15:47:22 -0400

    add DB

--- Model/Database.php
+++ Model/Database.php
@@ -3,11 +3,21 @@
 
 class Database
 {
-    //properties
-    private static $user = 'root';
-    private static $pass = '';
-    private static $dsn = 'mysql:host=localhost;dbname=task_management';
+    // //properties
+    // private static $user = 'root';
+    // private static $pass = '';
+    // private static $dsn = 'mysql:host=localhost;dbname=task_management';
 
+    // private static $dbcon;
+
+    // private function __construct()
+    // {
+    // }
+    
+    private static $user = 'bfaca630ffd158';
+    private static $pass = '2df07728ed2e994';
+    private static $dsn = 'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_96a828e3e46c53e;charset=utf8';
+    
     private static $dbcon;
 
     private function __construct()
